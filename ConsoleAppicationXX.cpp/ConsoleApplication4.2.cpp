f7df2ed620be58923b48233760e4516bb0f48e2c
n-vsc <nikola.simkin1912@gmail.com>
2021-06-02 20:06:02 +0300

    first transcription

---
+++ ConsoleAppicationXX.cpp/ConsoleApplication4.2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	double a, b;
+	cin >> a >> b;
+	// 5 + 7 = 12
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	cout << a << " " << b;
+		return 0;
+}
---
+++ ConsoleAppicationXX.cpp/ConsoleApplication8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <clocale>
+int main() {
+	setlocale(LC_ALL, "russian");
+	double a, b;
+	char n;
+	std:: cout << "введите выражение";
+	std:: cin >> a >> n >> b;
+	switch (n) {
+	case'+': std::cout << a << "+" << b << "=" << a + b;
+	case'*': std::cout << a << "*" << b << "=" << a * b; break;
+	case'-': std::cout << a << "-" << b << "=" << a - b; break;
+	case'/':
+		 if (b == 0) {
+			std::cout << "Знаменатель должен не равняться нулю"; break;
+		}
+		 else { std::cout << a << "/" << b << "=" << a / b; break; 
+		 }
+	default: std::cout << "Данный калькулятор может выполнять только действия сложения, умножения, вычитани и деления, только с вещественными числами ";
+	}
+	return 0;
+}
